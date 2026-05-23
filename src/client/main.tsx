be14fc919bee5f1b192961f8b4a1af8caeec6732
heetah <heetah6716@gmail.com>
2026-05-23 18:16:07 +0800

    feat: mount backend-loaded dashboard entry

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,20 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import { DashboardApp } from "./DashboardApp";
-import { buildDashboardViewModel } from "./dashboard-view-model";
+import { mountDashboard } from "./dashboard-bootstrap";
 
-const viewModel = buildDashboardViewModel({
-  simulationBalance: 10_000,
-  baseCurrency: "USD",
-  dataDate: "2026-05-23",
-  requestedCurrencies: ["USD", "EUR", "JPY", "TWD", "GBP", "CNY", "SGD"],
-  supportedCurrencies: {},
-  latestRates: {},
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+void mountDashboard({
+  render: (node) => root.render(<React.StrictMode>{node}</React.StrictMode>),
 });
-
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <DashboardApp viewModel={viewModel} />
-  </React.StrictMode>,
-);
